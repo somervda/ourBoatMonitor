1fce7b568c87f8b63cb3e2dd91e6bc5054d683b5
David <39265971+somervda@users.noreply.github.com>
2021-03-28 14:23:27 -0400

    Added a big try catch for main.py to recover fromm errors. Also post the freememeory to backend

--- uPython/main.py
+++ uPython/main.py
@@ -7,6 +7,7 @@ from pytrack import Pytrack
 from LIS2HH12 import LIS2HH12
 import time
 import gc
+import sys
 import bme280
 from ADS1115 import ADS1115
 
@@ -34,84 +35,95 @@ adc0VRatio = 0.0909
 #  flag to go into sleep loop (Turn off for testing)
 doSleep = True
 
-py = Pytrack()
-acc = LIS2HH12()
+try:
 
-print("")
+    py = Pytrack()
+    acc = LIS2HH12()
 
-# Set deep sleep parameters
-py.setup_int_wake_up(True, False)
-# Turn off accelerometer
-acc.set_odr(0)
-gc.collect()
+    print("")
 
-# ******************************************************************
-# Collect sensor data, append to the dataList array to be sent via LTE
-# ******************************************************************
-dataList = []
+    # Set deep sleep parameters
+    py.setup_int_wake_up(True, False)
+    # Turn off accelerometer
+    acc.set_odr(0)
+    gc.collect()
 
-# Get the Lithium Ion (Li-ion) battery voltage
-LIVoltage = py.read_battery_voltage()
-dataList.append(("LIVoltage", LIVoltage))
-print("LIVoltage", LIVoltage)
+    # ******************************************************************
+    # Collect sensor data, append to the dataList array to be sent via LTE
+    # ******************************************************************
+    dataList = []
 
-#  Include GPS reading
-if includeGPS:
-    print("Getting GPS...")
-    #  Get GPS data from pytrack board
-    gps = pytrackHelper.getGPS(py, 300)
-    if (gps[0] is not None and gps[1] is not None):
-        # Create a list of key value pairs to be
-        # sent by LTE to hologram
-        dataList.append(("lat", gps[0]))
-        dataList.append(("lng", gps[1]))
+    # Get the Lithium Ion (Li-ion) battery voltage
+    LIVoltage = py.read_battery_voltage()
+    dataList.append(("LIVoltage", LIVoltage))
+    print("LIVoltage", LIVoltage)
+
+    #  Include GPS reading
+    if includeGPS:
+        print("Getting GPS...")
+        #  Get GPS data from pytrack board
+        gps = pytrackHelper.getGPS(py, 300)
+        if (gps[0] is not None and gps[1] is not None):
+            # Create a list of key value pairs to be
+            # sent by LTE to hologram
+            dataList.append(("lat", gps[0]))
+            dataList.append(("lng", gps[1]))
+        else:
+            dataList.append(("lat", 0))
+            dataList.append(("lng", 0))
     else:
         dataList.append(("lat", 0))
         dataList.append(("lng", 0))
-else:
-    dataList.append(("lat", 0))
-    dataList.append(("lng", 0))
 
-# Use i2c bus 1 for additional i2c devices (Otherwise using Bus 0 messes up the deep sleep processing)
-i2c = I2C(1, I2C.MASTER, pins=('P10', 'P9'), baudrate=100000)
-#  Include the BME280 sensor for Temperature, Presure and humidity readings
-if includeBME280:
-    print("BME280")
-    bme = bme280.BME280(i2c=i2c)
-    t, p, h = bme.values
-    dataList.append(("Pressure", p))
-    dataList.append(("Temperature", t))
-    dataList.append(("Humidity", h))
-else:
-    dataList.append(("Pressure", 0))
-    dataList.append(("Temperature", 0))
-    dataList.append(("Humidity", 0))
+    # Use i2c bus 1 for additional i2c devices (Otherwise using Bus 0 messes up the deep sleep processing)
+    i2c = I2C(1, I2C.MASTER, pins=('P10', 'P9'), baudrate=100000)
+    #  Include the BME280 sensor for Temperature, Presure and humidity readings
+    if includeBME280:
+        print("BME280")
+        bme = bme280.BME280(i2c=i2c)
+        t, p, h = bme.values
+        dataList.append(("Pressure", p))
+        dataList.append(("Temperature", t))
+        dataList.append(("Humidity", h))
+    else:
+        dataList.append(("Pressure", 0))
+        dataList.append(("Temperature", 0))
+        dataList.append(("Humidity", 0))
 
-#  Include ADS1115 sensor for house battery voltage and bilge water level switch settings
-if includeADS1115:
-    adc = ADS1115(i2c, address=0x48)
-    print("adc.get_voltage(0)", adc.get_voltage(0))
-    dataList.append(("ADC0", adc.get_voltage(0)/adc0VRatio))
-    dataList.append(("ADC1", adc.get_voltage(1)))
-else:
-    dataList.append(("ADC0", -99))
-    dataList.append(("ADC1", -99))
+    #  Include ADS1115 sensor for house battery voltage and bilge water level switch settings
+    if includeADS1115:
+        adc = ADS1115(i2c, address=0x48)
+        print("adc.get_voltage(0)", adc.get_voltage(0))
+        dataList.append(("ADC0", adc.get_voltage(0)/adc0VRatio))
+        dataList.append(("ADC1", adc.get_voltage(1)))
+    else:
+        dataList.append(("ADC0", -99))
+        dataList.append(("ADC1", -99))
 
-# Turn off extra i2c devices before deep sleep
-i2c.deinit()
+    # Turn off extra i2c devices before deep sleep
+    i2c.deinit()
 
-print("dataList:", dataList)
+    dataList.append(("GC", gc.mem_free()))
 
-# Connect to LTE and send the list of data items and hologram device key
-if sendData:
-    lteHelper.sendData(dataList, "lQ6Gjc$n")
+    print("dataList:", dataList)
 
-if doSleep:
-    # Go into low power sleep
-    print("Deep sleep for %d seconds..." % (DEEP_SLEEP_SECONDS))
+    # Connect to LTE and send the list of data items and hologram device key
+    if sendData:
+        lteHelper.sendData(dataList, "lQ6Gjc$n")
 
-    time.sleep(1)
-    py.setup_sleep(DEEP_SLEEP_SECONDS)
-    py.go_to_sleep(gps=False)
-else:
-    print("No sleep - ending...")
+    if doSleep:
+        # Go into low power sleep
+        print("Deep sleep for %d seconds..." %
+              (DEEP_SLEEP_SECONDS))
+        time.sleep(1)
+        py.setup_sleep(DEEP_SLEEP_SECONDS)
+        py.go_to_sleep(gps=False)
+    else:
+        print("No sleep - ending...")
+except Exception as e:
+    # Any exceptions we reboot
+    sys.print_exception(e)
+    print("Waiting 60 seconds to reboot...")
+    time.sleep(60)
+    print('Reboot')
+    machine.reset()
